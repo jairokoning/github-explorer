524f3702d123d3e2c80843fb7003289894150c36
Jairo Koning <jairo.koning@gmail.com>
2022-02-13 21:51:27 -0300

    feat: component repository list

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,24 @@
+export function RepositoryList() {
+  return (
+    <section className="respository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
