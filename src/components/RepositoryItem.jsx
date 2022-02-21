2d843bffa4d4e9f96863f1eef281144e820c85fa
Jairo Koning <jairo.koning@gmail.com>
2022-02-21 07:28:52 -0300

    feat: list repositories from github api

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository?.name ?? 'Default'}</strong>
+      <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
-      <a href={props.repository.link}>{props.repository.link}</a>
+      <a href={props.repository.html_url}>{props.repository.html_url}</a>
     </li>
   )
 }
